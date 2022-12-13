cb244eb197b404ff1a43bdcdbd6c2b37c16128ba
vengadesh-15127 <vkvengadesh2@gmail.com>
2022-12-13 18:21:06 +0530

    five

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = ()=>{
         <>
         <div >
             <h1>About us</h1>
-            <p>Customers who view your About Us page will spend 22.5% more than those that don't.
+            <p>Hello !!!!!,Customers who view your About Us page will spend 22.5% more than those that don't.
                  And, according to a study by KoMarketing, 52% of users report that the About Us page is the first thing they want to see when they visit a website.</p>
         </div>
         </>
