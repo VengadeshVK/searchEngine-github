f33a3fc6749e465245f4ed7bacc54fc6c93b7ccc
vengadesh-15127 <vkvengadesh2@gmail.com>
2022-12-13 21:44:16 +0530

    seven

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,7 @@ const contact = ()=>{
         <div >
             
             <h1>Contact us</h1>
-            <p>Hello !!!!!!!!, awesome Contact Us page finds just the right balance between making it easy to reach 
+            <p>Hello !!!!!!!!!!!!, awesome Contact Us page finds just the right balance between making it easy to reach 
                 the company and sharing resources users can use to answer their questions right away.</p>
         </div>
         </>
